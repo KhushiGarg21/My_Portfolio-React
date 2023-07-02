ddee006bc419e5d536db8b05e588180cdf52a3d9
khushi <khushigarg21101@gmail.com>
2023-07-02 23:18:49 +0530

    chnges

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,28 @@
-import React from "react";
-
+import {React,useState} from "react";
+import axios from 'axios';
 const Contact = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+
+  const messageSendHandler = async () => {
+    try {
+      const resp = await axios.post(
+        "https://mern-cms-server.onrender.com/contact-page/add-query",
+        {
+          firstName: firstName,
+          email: email,
+          description: description,
+        }
+      );
+      console.log("response: ", resp.data.data);
+      setFirstName("");
+      setEmail("");
+      setDescription("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div
       name="contact"
@@ -23,23 +45,29 @@ const Contact = () => {
             <input
               type="text"
               name="name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
               placeholder="Enter your name"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <input
               type="text"
               name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter your email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <textarea
               name="message"
+              value = {description}
+              onChange={(e) => setDescription(e.target.value)}
               placeholder="Enter your message"
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
-            <button className="text-white font-bold bg-gradient-to-r from-gray-700 to-orange-800
+            <button onClick={messageSendHandler} className="text-white font-bold bg-gradient-to-r from-gray-700 to-orange-800
               px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Let's talk
             </button>
